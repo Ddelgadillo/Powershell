d1bcb35b4b2916bc382a2f8168784bb475d205b6
Diego Delgadillo <Ddelgadillo@users.noreply.github.com>
2022-08-24 11:37:18 -0500

    Update ExportAllUsersCSV.ps1

--- ExportAllUsersCSV.ps1
+++ ExportAllUsersCSV.ps1
@@ -1,1 +1,2 @@
+#Exports CSV file of ALL users from an ON-PREM ACtive directory. (Name, city, Email, Number, Department, Title)
 Get-ADUser -Filter * -Properties * | Select-Object Name, City, EmailAddress, telephonenumber, Department, Title | export-csv -path C:\Users\ddelgadillo\Documents\ParksiteUserExport.csv
